7527e8ac66fa9ced23d39dcf6d734874f50be63f
vansgpt <143675159+vansgpt@users.noreply.github.com>
2023-09-13 09:17:40 +0700

    update files

--- resources/views/welcome.kita.php
+++ resources/views/welcome.kita.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="Kamu - PHP Framework">
-    <meta name="description" content="Kamu - PHP Framework">
+    <meta name="title" content="Muhavan - PHP Framework">
+    <meta name="description" content="Muhavan - PHP Framework">
     <meta name="theme-color" content="#ffffff">
     <meta name="color-scheme" content="light">
-    <title>Kamu - PHP Framework</title>
+    <title>Muhavan - PHP Framework</title>
     <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('kamu.png') }}">
 </head>
@@ -16,7 +16,7 @@
 <body class="font-sans m-0 bg-gradient-to-br from-indigo-300 to-purple-400">
     <div class="flex h-screen justify-center items-center">
         <div class="text-slate-600 text-center bg-white p-8 rounded-xl drop-shadow-2xl">
-            <h1 class="inline font-medium text-4xl">Kamu |</h1>
+            <h1 class="inline font-medium text-4xl">Muhavan |</h1>
             <p class="inline font-medium text-2xl ml-2">{{ $data }}</p>
             <small class="block mt-4" id="information"></small>
         </div>
