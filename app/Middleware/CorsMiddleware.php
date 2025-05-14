a36b9f36579a1a5d4f6313161dc8e2368d7b78a7
muhavan <muhammad400809@gmail.com>
2025-05-14 23:48:48 +0700

    updates

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -13,7 +13,7 @@ public function handle(Request $request, Closure $next)
     // Log untuk memastikan middleware dijalankan
     error_log("CORS Middleware dijalankan");
 
-    header('Access-Control-Allow-Origin: https://muhavan.github.io');
+    header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, Token');
